0251e6c91e90f03d8591d51f0ea981dabc1718e5
Horacio Guzmán Parra <hgp691@gmail.com>
2017-08-15 22:48:49 -0500

    Initial Commit

---
+++ TestHero/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestHero
+//
+//  Created by Horacio Guzmán Parra on 15/08/17.
+//  Copyright © 2017 Horacio Guzmán Parra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
